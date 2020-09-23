644c20453f8230ef9f260a606f80ca5b43a13ab7
_ <anabel.quispe@unmsm.edu.pe>
2020-09-23 16:29:35 -0500

    Segunda version

--- Proyecto.R
+++ Proyecto.R
@@ -3,52 +3,61 @@ setwd("C:/Users/Adherly/Desktop/Proyecto de Progra")
 
 ## Cargar librerias
 library(sf)
-library(raster)
 library(mapview)
-library(dplyr)
 library(sp)
+library(raster)
+library(rgee)
+library(dplyr)
 library(mapedit)
 
-Cargar los shapes de provincia
+#Cargar los shapes de provincia
 prov <- st_read("Data/Provincias.shp")
 
-Cargar los raster previamente descargados
+# Cargar los raster previamente descargados
 dem1 <- raster("Data/ASTGTM_S18W071_dem.tif") 
 dem2 <- raster("Data/ASTGTM_S18W072_dem.tif")
 
-3. Unir 2 raster con mergedem <- merge(dem1, dem2)
+# Unir 2 raster con mergedem <- merge(dem1, dem2)
 dem <- merge(dem1, dem2)
-mapview(list(dem, prov))
+mapview(list(dem1, prov))
 
-4. Filtrar solo la provincia deseada
+# Filtrar solo la provincia deseada
 ilo <- dis %>% 
-  filter( DISCOD98 == "180301" )
+  filter( DISCOD98 == "180301")
 
-5. Comprobar si es el area deseada
+# Comprobar si es el area deseada
 mapview(list(dem, ilo))
 
-6. Importante: Objeto Simple Feature a Spatial: as(tu simple feature, "spatial")
+# Importante: Objeto Simple Feature a Spatial: as(tu simple feature, "spatial")
 sp_ilo <- as(ilo, "Spatial")
 
-7. Hacer una máscara para nuestra región
+# Hacer una máscara para nuestra región
 ilo_mask<- mask(dem, sp_ilo)
 plot(ilo_mask)
 
 ilo_crop <- crop(dem, sp_ilo) 
 plot(ilo_crop) 
-9. Hacer mask del crop
+
+# Hacer mask del crop
 ilo_dem <- mask(ilo_crop, sp_ilo) 
 plot(ilo_dem)
 
-Conviertiendo un objeto sf a ee con la librería rgee para luego extrar datos.
-area_ee <- sf_as_ee(area_sf)
-library(rgee)
-12. Extrayendo datos con rgee
-pro_clim <- ee.ImageCollection("NOAA/CDR/ATMOS_NEAR_SURFACE/V2")
+#Hallar la altitud media para 
+altitud_media <- raster::extract(ilo_dem, sp_ilo, fun = mean)
 
-install.packages("rgee")
+# Ahora con el uso de mapview y editMap seleccionaremos una area relativa
+area <- mapview(ilo_dem) %>% editMap()    
+area_sf <- area$all
+plot(area_sf)
 
-remotes::install_github("r-spatial/rgee")
+mapview(list(ilo_dem, area_sf))
+# Conviertiendo un objeto sf a ee, ee_Initialize()
+area_ee <- sf_as_ee(area_sf)       ##No pudimos avanzar 
 
-
-
+# Extraer datos de ee_pp: pp_ins
+pp_ins <- ee$ImageCollection("JAXA/GPM_L3/GSMaP/v6/operational")$ 
+  filterDate("2018-08-06", "2018-08-07")$ first()
+pp_stack <- ee_as_raster(imag = ee_cc, region = area_ee$geometry())
+pp_area_ins <- pp_stack[[1]]
+plot(pp_area_ins)
+mapview(list(pp_area_ins, ilo_dem))
